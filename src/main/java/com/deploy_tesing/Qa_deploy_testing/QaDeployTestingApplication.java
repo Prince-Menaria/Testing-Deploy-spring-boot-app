a948a2364515c652405d7c7b42d4efa2b443caf5
Prince-Menaria <princemenaria04@gmail.com>
2026-09-10 14:32:07 +0530

    Initial commit

---
+++ src/main/java/com/deploy_tesing/Qa_deploy_testing/QaDeployTestingApplication.java
@@ -0,0 +1,13 @@
+package com.deploy_tesing.Qa_deploy_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaDeployTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaDeployTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploy_tesing/Qa_deploy_testing/QaDeployTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy_tesing.Qa_deploy_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaDeployTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
